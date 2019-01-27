55eb723fc4fc9940a5c8310d3c42fd062e46b312
Moritz Morgenroth <development@moritzmorgenroth.de>
2019-01-27 11:46:56 +0100

    Succefully fetched data

--- lib/main.dart
+++ lib/main.dart
@@ -1,174 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:realtimequiz/ui/quiz.dart';
+import './ui/animal_quiz.dart';
 
-
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'dart:async';
-
-import 'questions.dart';
-import 'questions_dissmissible.dart';
-import 'questions_buttons.dart';
-import 'questions_pagable.dart';
-
-void main() {
-  runApp(new QuizApp());
+void main(){
+  runApp(
+      new MaterialApp(
+        home: new AnimalQuiz(),
+      )
+  );
 }
 
-class QuizApp extends StatelessWidget {
-  final viewChangeStreamController = new StreamController<Null>();
-
-  _quizViewChange() {
-    viewChangeStreamController.add(null);
+class AnimalQuiz extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new AnimalQuizState();
   }
+}
 
+class AnimalQuizState extends State<AnimalQuiz>{
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Quiz',
-      theme: new ThemeData(
-        primaryColor: Colors.white,
-        primarySwatch: Colors.blue,
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Multiple Choice Quiz"),
+        backgroundColor: Colors.blue,
       ),
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Flutter Quiz'),
-          elevation: 0.0,
-          actions: [new IconButton(
-            icon: new Icon(Icons.navigate_next),
-            tooltip: 'Change quiz view',
-            onPressed: _quizViewChange,
-          )],
+
+
+      body: new Container(
+        margin: const EdgeInsets.all(15.0),
+        child: new Column(
+
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+
+            new MaterialButton(
+                height: 50.0,
+                color: Colors.green,
+                onPressed: startQuiz,
+                child: new Text("Quiz 1",
+                  style: new TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.white
+                  ),)
+            )
+          ],
         ),
-        body: new Padding(
-          padding: new EdgeInsets.only(top: 5.0,),
-          child: new QuizPage(viewChangeStreamController.stream),
-        ),
-        backgroundColor: Colors.white,
       ),
+
+
     );
+  }
+
+  void startQuiz(){
+    Navigator.push(context, new MaterialPageRoute(builder: (context)=> new QuizPage()));
   }
 }
 
-enum QuestionView { buttons, dissmissible, pagable }
-
-class QuizPage extends StatefulWidget {
-  QuizPage(this.viewChangeStream);
-  final Stream<Null> viewChangeStream;
-
-  @override
-  createState() => new QuizPageState();
-}
-
-class QuizPageState extends State<QuizPage> {
-  var score = 0;
-  var questions = <Question>[];
-  var questionView = QuestionView.dissmissible;
-  StreamSubscription viewChangeSubscription;
-
-  @override
-  initState() {
-    super.initState();
-    _loadQuestions();
-    viewChangeSubscription = widget.viewChangeStream.listen(
-            (_) => changeQuestionsView()
-    );
-  }
-
-  @override
-  dispose() {
-    super.dispose();
-    if (widget.viewChangeStream != null) {
-      viewChangeSubscription.cancel();
-    }
-  }
-
-  _loadQuestions() async {
-    final loadedQuestions = await loadQuestionsLocally();
-    setState(() => questions = loadedQuestions);
-  }
-
-  _resetGame() => setState(() {
-    score = 0;
-    _loadQuestions();
-  });
-
-  _answerQuestion(Question question, bool answer) async {
-    setState(() => question.answer == answer ? score++ : score--);
-
-    questions.remove(question);
-    if (questions.length == 0) {
-      await _gameOverDialog();
-      _resetGame();
-    }
-  }
-
-  _gameOverDialog() async {
-    return showDialog<Null>(
-      context: context,
-      child: new AlertDialog(
-        title: new Text('Game Over'),
-        content: new Text('You scored $score'),
-        actions: <Widget>[
-          new FlatButton(
-            child: new Text('Play Again'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  /// Change between the different question views
-  changeQuestionsView() {
-    setState(() =>
-    questionView = QuestionView.values[
-    (questionView.index + 1) % QuestionView.values.length]
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    Widget questionWidget;
-    switch(questionView) {
-      case QuestionView.dissmissible:
-        questionWidget = new QuestionsDissmissible(questions, _answerQuestion);
-        break;
-      case QuestionView.buttons:
-        questionWidget = new QuestionsButtons(questions, _answerQuestion);
-        break;
-      case QuestionView.pagable:
-        questionWidget = new QuestionsPageable(questions, _answerQuestion);
-        break;
-    }
-    return new Column(
-      children: [
-        new Score(score),
-        new Expanded(
-          child: questionWidget,
-        ),
-      ],
-    );
-  }
-}
-
-class Score extends StatelessWidget {
-  Score(this.score);
-  final int score;
-
-  @override
-  Widget build(BuildContext context) =>
-      new Center(
-        child: new Text(
-          'Score: $score',
-          style: Theme.of(context).textTheme.display1,
-        ),
-      );
-
-}
 
 //void main() => runApp(MyApp());
 //
